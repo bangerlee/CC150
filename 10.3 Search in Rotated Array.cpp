42c0f1dbefdb8cf93f03300a02bff55b00ba84b4
lixin <bangerlee@gmail.com>
2019-04-03 15:36:41 +0800

    Create 10.3 Search in Rotated Array.cpp

---
+++ 10.3 Search in Rotated Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        if(nums.size()==0) return -1;
+        while(i<j){
+            int mid=(i+j)/2;
+            if(nums[mid]>nums[j]){
+                if(nums[mid]<target||target<=nums[j]) i=mid+1;
+                else j=mid;
+            }
+            else{
+                if(nums[mid]<target&&target<=nums[j]) i=mid+1;
+                else j=mid;
+            }
+        }
+        if(i==j&&nums[i]!=target) return -1;
+        return i;
+    }
+};
