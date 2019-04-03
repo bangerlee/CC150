61260c22c394811831f5f7ec509ec8ce8c8cb7e6
lixin <bangerlee@gmail.com>
2019-04-03 16:26:00 +0800

    Create 10.11 Peaks and Valleys.cpp

---
+++ 10.11 Peaks and Valleys.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(i%2==1){
+                if(nums[i-1]>nums[i]) swap(nums[i-1],nums[i]);
+            }
+            else{
+                if(i!=0&&nums[i]>nums[i-1]) swap(nums[i],nums[i-1]);
+            }
+        }
+    }
+};
